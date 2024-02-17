717496d90762b42a4d648968d785034cd91061b9
dm <heydruavh@gmail.com>
2024-02-17 23:58:13 +0530

    built counter project

---
+++ Counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+  function addVal() {
+    counter += 1;
+    setCounter(counter);
+  }
+  function subVal() {
+    counter -= 1;
+    setCounter(counter);
+  }
+  function resetVal() {
+    counter = 0;
+    setCounter(counter);
+  }
+  if (counter < 0) {
+    counter = 0;
+  }
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={addVal}>Add</button>
+      <button onClick={subVal}>Subtract</button>
+      <button onClick={resetVal}>Reset</button>
+    </>
+  );
+}
+
+export default App;
