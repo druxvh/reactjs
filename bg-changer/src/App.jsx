e642bcbc24e55c52c310f48306d5e16e315e6485
dm <heydruavh@gmail.com>
2024-02-20 00:26:07 +0530

    add bgcolor project

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -2,17 +2,73 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("gray");
+
   return (
-    <div className="w-full bg-gray-300 py-3 px-3 mx-9 rounded-lg fixed bottom-1">
-      <button className=" bg-red-500 px-5 py-2 mx-3 rounded-full text-white">
-        Red
-      </button>
-      <button className=" bg-green-500 px-5 py-2 mx-3 rounded-full text-white">
-        green
-      </button>
-      <button className=" bg-yellow-500 px-5 py-2 mx-3 rounded-full text-white">
-        yellow
-      </button>
+    <div
+      className="w-full h-screen flex justify-center items-center"
+      style={{ backgroundColor: color }}
+    >
+      {" "}
+      <p className=" font-semibold text-2xl text-gray-300">
+        Click any button below to change the background color of the screen
+      </p>
+      <div className="w-fit bg-gray-300 py-3 px-3 my-3 rounded-full fixed bottom-1">
+        <button
+          onClick={() => {
+            setColor("red");
+          }}
+          className="px-5 py-2 mx-3 rounded-full text-white"
+          style={{ backgroundColor: "red" }}
+        >
+          Red
+        </button>
+        <button
+          onClick={() => {
+            setColor("green");
+          }}
+          className="px-5 py-2 mx-3 rounded-full text-white"
+          style={{ backgroundColor: "green" }}
+        >
+          Green
+        </button>
+        <button
+          onClick={() => {
+            setColor("yellow");
+          }}
+          className="px-5 py-2 mx-3 rounded-full text-white"
+          style={{ backgroundColor: "yellow" }}
+        >
+          Yellow
+        </button>
+        <button
+          onClick={() => {
+            setColor("pink");
+          }}
+          className="px-5 py-2 mx-3 rounded-full text-white"
+          style={{ backgroundColor: "pink" }}
+        >
+          Pink
+        </button>
+        <button
+          onClick={() => {
+            setColor("cyan");
+          }}
+          className="px-5 py-2 mx-3 rounded-full text-white"
+          style={{ backgroundColor: "cyan" }}
+        >
+          Cyan
+        </button>
+        <button
+          onClick={() => {
+            setColor("blue");
+          }}
+          className="px-5 py-2 mx-3 rounded-full text-white"
+          style={{ backgroundColor: "blue" }}
+        >
+          Blue
+        </button>
+      </div>
     </div>
   );
 }
