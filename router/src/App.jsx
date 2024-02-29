77c7fef96ca4dfc6ac48371fea5fa1db15338225
dm <heydruavh@gmail.com>
2024-02-29 23:56:12 +0530

    learned react router

---
+++ router/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
